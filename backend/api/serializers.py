b630ebb1645a92aba126139b33b0bceaa7c4caf2
Tim Vögtli <tim.voegtli@hf-ict.info>
2023-01-17 11:28:26 +0100

    tasks endpoint and init vue.js

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Task
 from rest_framework import serializers
 
 
@@ -6,3 +7,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Task;
+        fields = ['id', 'name', 'start', 'end']
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
+from .models import Task
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import UserSerializer
+from .serializers import TaskSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all();
+    serializer_class = TaskSerializer;
